ee9021f45ef0bf580fc447133e9856d180d9dd75
Bonaventure Simeon <contact@bonaventure.org.ng>
2026-09-18 00:07:31 +0100

    Add legal rollout check to offline release runner

--- tools/run_release_checks.py
+++ tools/run_release_checks.py
@@ -23,6 +23,7 @@ CHECKS = [
     ROOT / "tools" / "check_runner_recovery_contract.py",
     ROOT / "tools" / "check_mt5_validation_contract.py",
     ROOT / "tools" / "check_api_transport_static.py",
+    ROOT / "tools" / "check_legal_rollout_static.py",
 ]
 
 

8daed0ad97d0fee6b416760e3e7eea84196a63b2
Bonaventure Simeon <contact@bonaventure.org.ng>
2026-09-17 23:49:17 +0100

    Run MT5 validation contract in aggregate release checks

--- tools/run_release_checks.py
+++ tools/run_release_checks.py
@@ -21,6 +21,7 @@ CHECKS = [
     ROOT / "tools" / "check_r5_adaptive.py",
     ROOT / "tools" / "check_r6_soak_lifecycle.py",
     ROOT / "tools" / "check_runner_recovery_contract.py",
+    ROOT / "tools" / "check_mt5_validation_contract.py",
     ROOT / "tools" / "check_api_transport_static.py",
 ]
 
